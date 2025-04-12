0d8fbd1c4339e9d4edbf8a8a6da574bf6a84c15e
ponfertato <ponfertato@ya.ru>
2025-04-12 20:58:12 +0300

    🎨migrate to src

--- component/ai-prompt/filter.ts
+++
@@ -1,9 +0,0 @@
-export async function FilterConversation(optionInformation: any) {
-      // Determine conversation style
-  const conversationStyle =
-  optionInformation.conversation === "normal"
-    ? "Gunakan gaya bahasa santai dan tidak baku, seperti manusia normal."
-    : "Gunakan bahasa baku.";
-
-    return conversationStyle;
-}
--- component/server-autoplay.ts
+++
@@ -1,6 +0,0 @@
-export async function AutoPlay(page: any) {
-  const playButtonSelector =
-    'button[aria-haspopup="true"][dropdowntoggle][class="btn btn-lg btn-success text-truncate flex-grow-1"]';
-  await page.waitForSelector(playButtonSelector, { visible: true });
-  await page.click(playButtonSelector);
-}
